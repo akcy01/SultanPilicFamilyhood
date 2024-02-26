c68716a2257bec105b4c8a8b7f3b3c1f2140aa05
Oğuz Akçay <98681852+akcy01@users.noreply.github.com>
2024-02-26 19:46:13 +0300

    About us container has created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,12 +6,27 @@ function Home() {
         <div className="homepage">
             <div className="homepage-container">
                 <img src="images/homePageIntroduction.png" />
+                <div className="vertical-Line1"></div>
                 <h1>SULTAN PİLİÇ</h1>
-                <p>Damak tadınıza uygun lezzetler.</p>
+                <h3>1997'den beri..</h3>
+                <p>Kuruluş yılımız olan 1997'den bu yana damak tadınıza uygun, en taze ürünlerimizi sizlere sunmaktayız.</p>
                 <button>Ürünleri Gör</button>
             </div>
+
             <div className="homepage-aboutUs">
-                Hakkımızda
+                <img src="images/aboutUsBackground.png" />
+                <hr className="horizontal-line1"/>
+                <div className="box1">
+                    <img src="images/boxImage1.jpg" alt="box-image1"/>
+                </div>
+                <div className="box2">
+                    <img src="images/boxImage2.jpg" alt="box-image2"/>
+                </div>
+                <div className="box3">
+                    <img src="images/boxImage3.jpg" alt="box-image3"/>
+                </div>
+                <hr className="horizontal-line2"/>
+                <h1>HAKKIMIZDA</h1>
             </div>
 
         </div>
