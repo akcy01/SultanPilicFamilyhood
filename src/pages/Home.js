17fafcdcb4bb7d4c132422a180df18fba03b40a4
Oğuz Akçay <98681852+akcy01@users.noreply.github.com>
2024-03-10 00:47:17 +0300

    HomePage responsive design has completed.(For Width 450px)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,13 +12,15 @@ function Home(props) {
 
     const defaultImageRoads = {
         homepageIntroductionBackground: "images/homePageIntroduction.png",
-        aboutUsBackground: "images/aboutUsBackground.png"
+        aboutUsBackground: "images/aboutUsBackground.png",
+        goalsAndMissionsBackground: "images/goalsAndMissions.png"
     };
 
     if (window.visualViewport.width <= 450) {
         mobileControls.isMobile = true;
-        defaultImageRoads.homepageIntroductionBackground = "images/homePageIntroductionMobile.png"
-        defaultImageRoads.aboutUsBackground = "images/aboutUsBackgroundMobile.png"
+        defaultImageRoads.homepageIntroductionBackground = "images/homePageIntroductionMobile.png";
+        defaultImageRoads.aboutUsBackground = "images/aboutUsBackgroundMobile.png";
+        defaultImageRoads.goalsAndMissionsBackground = "images/goalsAndMissionsMobile.jpg";
     }
     else
         mobileControls.isMobile = false;
@@ -58,9 +60,9 @@ function Home(props) {
                                 <h1>HAKKIMIZDA</h1>
                                 <p>
                                     1997 yılında kurulan işletmemiz, müşterilerine en taze ve kaliteli çiğ tavuk ürünleri sunma amacıyla yola çıktı. Geçen yıllar içinde, gastronomi dünyasındaki değişimlere ayak uydurarak ürün yelpazemizi genişlettik. Başlangıçta sadece çiğ tavuk satışıyla tanınan dükkanımız, günümüzde artık pişirim hizmeti ve çeşitli meze seçenekleriyle müşterilerine zengin bir deneyim sunuyor.Taze ve sağlıklı ürünlerimizle damak zevkine önem veren müşterilerimize hizmet vermekten gurur duyuyoruz. Kalite, hijyen, ve lezzet konularında titizlikle çalışarak, müşterilerimize güvenilir ve keyifli bir alışveriş deneyimi sunuyoruz.</p>
-                                    <button>Devamını Gör</button>
+                                <button>Devamını Gör</button>
                             </div>
-                            
+
                         ) : (
                             <div className="aboutUs-desktop-container">
                                 <h1>HAKKIMIZDA</h1>
@@ -71,25 +73,60 @@ function Home(props) {
                     </div>
 
                     <div className="homepage-goalsAndMissions">
-                        <img src="images/goalsAndMissions.png" />
+                        <img src={defaultImageRoads.goalsAndMissionsBackground} />
                         <div className="goals-container">
-                            <h1>HEDEFLERİMİZ VE MİSYONLARIMIZ</h1>
-                            <h3>asdfqweuırhqıwu qwıuerqwıue qwıueh qwerıu asdfqwe qw qwe qwer qwerqwteasdfas asdfasdfasdfasdf</h3>
-                            <div class="box">
-                                <h2>Tarihçe</h2>
-                                <hr class="inner-line" />
-                                <p>u bölüm kelimeleri doğru şekilde kullanmanıza yardımcı olacaktır. Bir kelimenin bağlam içinde nasıl kullanılacağını anlamak genellikle çeviriden daha kolaydır. Her sayfa birkaç örnek cümle ile gösterilmiştir ve istediğiniz zaman bir çeviri sayfasına geri dönebilirsiniz. Ayrıca, doğrudan bölümün dahili arama motorunu kullanarak da kelime arayabilirsiniz. 18 dilde örnekler bulacaksınız.Bu bölüm kelimeleri doğru şekilde kullanmanıza yardımcı olacaktır. Bir kelimenin bağlam içinde nasıl kullanılacağını anlamak genellikle çeviriden daha kolaydır. Her sayfa birkaç örnek cümle ile gösterilmiştir ve istediğiniz zaman bir çeviri sayfasına geri dönebilirsiniz. Ayrıca, doğrudan bölümün dahili arama motorunu kullanarak da kelime arayabilirsiniz. 18 dilde örnekler bulacaksınız.</p>
-                            </div>
-                            <div class="box">
-                                <h2>Misyonumuz</h2>
-                                <hr class="inner-line" />
-                                <p>u bölüm kelimeleri doğru şekilde kullanmanıza yardımcı olacaktır. Bir kelimenin bağlam içinde nasıl kullanılacağını anlamak genellikle çeviriden daha kolaydır. Her sayfa birkaç örnek cümle ile gösterilmiştir ve istediğiniz zaman bir çeviri sayfasına geri dönebilirsiniz. Ayrıca, doğrudan bölümün dahili arama motorunu kullanarak da kelime arayabilirsiniz. 18 dilde örnekler bulacaksınız.Bu bölüm kelimeleri doğru şekilde kullanmanıza yardımcı olacaktır. Bir kelimenin bağlam içinde nasıl kullanılacağını anlamak genellikle çeviriden daha kolaydır. Her sayfa birkaç örnek cümle ile gösterilmiştir ve istediğiniz zaman bir çeviri sayfasına geri dönebilirsiniz. Ayrıca, doğrudan bölümün dahili arama motorunu kullanarak da kelime arayabilirsiniz. 18 dilde örnekler bulacaksınız.</p>
-                            </div>
-                            <div class="box">
-                                <h2>Vizyonumuz</h2>
-                                <hr class="inner-line" />
-                                <p>u bölüm kelimeleri doğru şekilde kullanmanıza yardımcı olacaktır. Bir kelimenin bağlam içinde nasıl kullanılacağını anlamak genellikle çeviriden daha kolaydır. Her sayfa birkaç örnek cümle ile gösterilmiştir ve istediğiniz zaman bir çeviri sayfasına geri dönebilirsiniz. Ayrıca, doğrudan bölümün dahili arama motorunu kullanarak da kelime arayabilirsiniz. 18 dilde örnekler bulacaksınız.Bu bölüm kelimeleri doğru şekilde kullanmanıza yardımcı olacaktır. Bir kelimenin bağlam içinde nasıl kullanılacağını anlamak genellikle çeviriden daha kolaydır. Her sayfa birkaç örnek cümle ile gösterilmiştir ve istediğiniz zaman bir çeviri sayfasına geri dönebilirsiniz. Ayrıca, doğrudan bölümün dahili arama motorunu kullanarak da kelime arayabilirsiniz. 18 dilde örnekler bulacaksınız.</p>
-                            </div>
+                            {mobileControls.isMobile ? (
+                                <div className="goals-container-mobile">
+                                    <h1>VİZYON & MİSYON</h1>
+                                    <h3>Kalite ve sağlık odaklı ürünler, güvenilir hizmet anlayışımızla birleşiyor ve 1997'den beri müşterilerimize taze ve lezzetli ürünleri sunmanın gururunu yaşıyoruz.</h3>
+
+                                    <div className="mission-vision-container">
+                                        <div className="mission-header">
+                                            <div>MİSYON</div>
+                                        </div>
+                                        <div className="mission-field">
+                                            <p>
+                                                deneme deneme deneme deneme deneme deneme deneme deneme deneme
+                                            </p>
+                                        </div>
+
+                                        <div className="vision-header">
+                                            <div>VİZYON</div>
+                                        </div>
+                                        <div className="vision-field">
+                                            <p>
+                                                deneme deneme deneme deneme deneme deneme deneme deneme deneme
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <button>
+                                        <div>
+                                            Devamını Gör
+                                        </div>
+                                    </button>
+                                </div>
+                            ) : (
+                                <>
+                                    <h1>VİZYON & MİSYON</h1>
+                                    <h3>asdfqweuırhqıwu qwıuerqwıue qwıueh qwerıu asdfqwe qw qwe qwer qwerqwteasdfas asdfasdfasdfasdf</h3>
+                                    <div class="box">
+                                        <h2>Tarihçe</h2>
+                                        <hr class="inner-line" />
+                                        <p>u bölüm kelimeleri doğru şekilde kullanmanıza yardımcı olacaktır. Bir kelimenin bağlam içinde nasıl kullanılacağını anlamak genellikle çeviriden daha kolaydır. Her sayfa birkaç örnek cümle ile gösterilmiştir ve istediğiniz zaman bir çeviri sayfasına geri dönebilirsiniz. Ayrıca, doğrudan bölümün dahili arama motorunu kullanarak da kelime arayabilirsiniz. 18 dilde örnekler bulacaksınız.Bu bölüm kelimeleri doğru şekilde kullanmanıza yardımcı olacaktır. Bir kelimenin bağlam içinde nasıl kullanılacağını anlamak genellikle çeviriden daha kolaydır. Her sayfa birkaç örnek cümle ile gösterilmiştir ve istediğiniz zaman bir çeviri sayfasına geri dönebilirsiniz. Ayrıca, doğrudan bölümün dahili arama motorunu kullanarak da kelime arayabilirsiniz. 18 dilde örnekler bulacaksınız.</p>
+                                    </div>
+                                    <div class="box">
+                                        <h2>Misyonumuz</h2>
+                                        <hr class="inner-line" />
+                                        <p>u bölüm kelimeleri doğru şekilde kullanmanıza yardımcı olacaktır. Bir kelimenin bağlam içinde nasıl kullanılacağını anlamak genellikle çeviriden daha kolaydır. Her sayfa birkaç örnek cümle ile gösterilmiştir ve istediğiniz zaman bir çeviri sayfasına geri dönebilirsiniz. Ayrıca, doğrudan bölümün dahili arama motorunu kullanarak da kelime arayabilirsiniz. 18 dilde örnekler bulacaksınız.Bu bölüm kelimeleri doğru şekilde kullanmanıza yardımcı olacaktır. Bir kelimenin bağlam içinde nasıl kullanılacağını anlamak genellikle çeviriden daha kolaydır. Her sayfa birkaç örnek cümle ile gösterilmiştir ve istediğiniz zaman bir çeviri sayfasına geri dönebilirsiniz. Ayrıca, doğrudan bölümün dahili arama motorunu kullanarak da kelime arayabilirsiniz. 18 dilde örnekler bulacaksınız.</p>
+                                    </div>
+                                    <div class="box">
+                                        <h2>Vizyonumuz</h2>
+                                        <hr class="inner-line" />
+                                        <p>u bölüm kelimeleri doğru şekilde kullanmanıza yardımcı olacaktır. Bir kelimenin bağlam içinde nasıl kullanılacağını anlamak genellikle çeviriden daha kolaydır. Her sayfa birkaç örnek cümle ile gösterilmiştir ve istediğiniz zaman bir çeviri sayfasına geri dönebilirsiniz. Ayrıca, doğrudan bölümün dahili arama motorunu kullanarak da kelime arayabilirsiniz. 18 dilde örnekler bulacaksınız.Bu bölüm kelimeleri doğru şekilde kullanmanıza yardımcı olacaktır. Bir kelimenin bağlam içinde nasıl kullanılacağını anlamak genellikle çeviriden daha kolaydır. Her sayfa birkaç örnek cümle ile gösterilmiştir ve istediğiniz zaman bir çeviri sayfasına geri dönebilirsiniz. Ayrıca, doğrudan bölümün dahili arama motorunu kullanarak da kelime arayabilirsiniz. 18 dilde örnekler bulacaksınız.</p>
+                                    </div>
+                                </>
+                            )}
+
                         </div>
                     </div>
 
