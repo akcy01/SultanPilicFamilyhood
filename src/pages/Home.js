54c6b7ab1928c54a9736d325e5681582a03a4e4d
Oğuz Akçay <98681852+akcy01@users.noreply.github.com>
2024-04-10 16:31:35 +0300

    About us has completed.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,7 +16,8 @@ function Home(props) {
     const defaultImageRoads = {
         homepageIntroductionBackground: "images/homePageIntroduction.png",
         aboutUsBackground: "images/aboutUsBackground.png",
-        goalsAndMissionsBackground: "images/goalsAndMissions.png"
+        goalsAndMissionsBackground: "images/goalsAndMissions.png",
+        homeBackground: "images/homePage.png"
     };
 
     if (receivedDataAtNavbar.data !== null) {
@@ -39,118 +40,55 @@ function Home(props) {
     return (
         <div>
             {controls.showHomePage && (
-                <div className="homepage">
-                    <div className="homepage-container">
-                        <img src={defaultImageRoads.homepageIntroductionBackground} />
-                        <div className="vertical-Line-container">
-                            <div className="vertical-Line1"></div>
+                <>
+                    <section className="home" id="home">
+                        <div className="home-text">
+                            <h1>Gelin Yiyin ve gerçek lezzetin tadını çıkarın. </h1>
+                            <a href="#" className="btn">Devamını Gör</a>
                         </div>
 
-                        <h1>SULTAN PİLİÇ</h1>
-                        <h3>1997'den beri..</h3>
-                        <p>Kuruluş yılımız olan 1997'den bu yana damak tadınıza uygun, en taze ürünlerimizi sizlere sunmaktayız.</p>
-                        <button>Ürünleri Gör</button>
-                    </div>
-
-                    <div className="homepage-aboutUs">
-
-                        <img src={defaultImageRoads.aboutUsBackground} />
-
-                        <div className="horizontal-line1"></div>
-                        <div className="box1">
-                            <img src="images/boxImage1.jpg" alt="box-image1" />
+                        <div className="home-img">
+                            <img src={defaultImageRoads.homeBackground} />
                         </div>
-                        <div className="box2">
-                            <img src="images/boxImage2.jpg" alt="box-image2" />
+                    </section>
+
+                    <section className="container-cont">
+                        <div className="container-box">
+                            <img src="images/c1.png" />
+                            <h3>10:00 - 22:00</h3>
+                            <a href="*"> Çalışma Saatleri </a>
                         </div>
-                        <div className="box3">
-                            <img src="images/boxImage3.jpg" alt="box-image3" />
+
+                        <div className="container-box">
+                            <img src="images/c2.png" />
+                            <h3>Shell Yolu Üzeri</h3>
+                            <a href="*"> Konuma Git </a>
                         </div>
-                        <hr className="horizontal-line2" />
 
-                        {mobileControls.isMobile ? (
-                            <div className="aboutUs-mobile-container">
-                                <div className="aboutUs-mobile-contains">
-                                    <h1>HAKKIMIZDA</h1>
-                                    <p>
-                                        1997 yılında kurulan işletmemiz, müşterilerine en taze ve kaliteli çiğ tavuk ürünleri sunma amacıyla yola çıktı. Geçen yıllar içinde, gastronomi dünyasındaki değişimlere ayak uydurarak ürün yelpazemizi genişlettik. Başlangıçta sadece çiğ tavuk satışıyla tanınan dükkanımız, günümüzde artık pişirim hizmeti ve çeşitli meze seçenekleriyle müşterilerine zengin bir deneyim sunuyor.
-                                    </p>
-                                </div>
-                                <button>
-                                    <div>
-                                        Devamını Gör
-                                    </div>
-                                </button>
-                            </div>
-                        ) : (
-                            <div className="aboutUs-desktop-container">
-                                <h1>HAKKIMIZDA</h1>
-                                <p>
-                                    1997 yılında kurulan işletmemiz, müşterilerine en taze ve kaliteli çiğ tavuk ürünleri sunma amacıyla yola çıktı. Geçen yıllar içinde, gastronomi dünyasındaki değişimlere ayak uydurarak ürün yelpazemizi genişlettik. Başlangıçta sadece çiğ tavuk satışıyla tanınan dükkanımız, günümüzde artık pişirim hizmeti ve çeşitli meze seçenekleriyle müşterilerine zengin bir deneyim sunuyor.Taze ve sağlıklı ürünlerimizle damak zevkine önem veren müşterilerimize hizmet vermekten gurur duyuyoruz. Kalite, hijyen, ve lezzet konularında titizlikle çalışarak, müşterilerimize güvenilir ve keyifli bir alışveriş deneyimi sunuyoruz.</p>
-                            </div>
-                        )}
-                    </div>
-
-                    <div className="homepage-goalsAndMissions">
-                        <img src={defaultImageRoads.goalsAndMissionsBackground} />
-                        <div className="goals-container">
-                            {mobileControls.isMobile ? (
-                                <div className="goals-container-mobile">
-                                    <h1>VİZYON & MİSYON</h1>
-                                    <h3>Kalite ve sağlık odaklı ürünler, güvenilir hizmet anlayışımızla birleşiyor ve 1997'den beri müşterilerimize taze ve lezzetli ürünleri sunmanın gururunu yaşıyoruz.</h3>
-
-                                    <div className="mission-vision-container">
-                                        <div className="mission-header">
-                                            <div>MİSYON</div>
-                                        </div>
-                                        <div className="mission-field">
-                                            <p>
-                                                deneme deneme deneme deneme deneme deneme deneme deneme deneme
-                                            </p>
-                                        </div>
-
-                                        <div className="vision-header">
-                                            <div>VİZYON</div>
-                                        </div>
-                                        <div className="vision-field">
-                                            <p>
-                                                deneme deneme deneme deneme deneme deneme deneme deneme deneme
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <button>
-                                        <div>
-                                            Devamını Gör
-                                        </div>
-                                    </button>
-                                </div>
-                            ) : (
-                                <div className="goals-container-mobile">
-                                    <h1>VİZYON & MİSYON</h1>
-                                    <h3>asdfqweuırhqıwu qwıuerqwıue qwıueh qwerıu asdfqwe qw qwe qwer qwerqwteasdfas asdfasdfasdfasdf</h3>
-                                    <div class="box">
-                                        <h2>Tarihçe</h2>
-                                        <hr class="inner-line" />
-                                        <p>u bölüm kelimeleri doğru şekilde kullanmanıza yardımcı olacaktır. Bir kelimenin bağlam içinde nasıl kullanılacağını anlamak genellikle çeviriden daha kolaydır. Her sayfa birkaç örnek cümle ile gösterilmiştir ve istediğiniz zaman bir çeviri sayfasına geri dönebilirsiniz. Ayrıca, doğrudan bölümün dahili arama motorunu kullanarak da kelime arayabilirsiniz. 18 dilde örnekler bulacaksınız.Bu bölüm kelimeleri doğru şekilde kullanmanıza yardımcı olacaktır. Bir kelimenin bağlam içinde nasıl kullanılacağını anlamak genellikle çeviriden daha kolaydır. Her sayfa birkaç örnek cümle ile gösterilmiştir ve istediğiniz zaman bir çeviri sayfasına geri dönebilirsiniz. Ayrıca, doğrudan bölümün dahili arama motorunu kullanarak da kelime arayabilirsiniz. 18 dilde örnekler bulacaksınız.</p>
-                                    </div>
-                                    <div class="box">
-                                        <h2>Misyonumuz</h2>
-                                        <hr class="inner-line" />
-                                        <p>u bölüm kelimeleri doğru şekilde kullanmanıza yardımcı olacaktır. Bir kelimenin bağlam içinde nasıl kullanılacağını anlamak genellikle çeviriden daha kolaydır. Her sayfa birkaç örnek cümle ile gösterilmiştir ve istediğiniz zaman bir çeviri sayfasına geri dönebilirsiniz. Ayrıca, doğrudan bölümün dahili arama motorunu kullanarak da kelime arayabilirsiniz. 18 dilde örnekler bulacaksınız.Bu bölüm kelimeleri doğru şekilde kullanmanıza yardımcı olacaktır. Bir kelimenin bağlam içinde nasıl kullanılacağını anlamak genellikle çeviriden daha kolaydır. Her sayfa birkaç örnek cümle ile gösterilmiştir ve istediğiniz zaman bir çeviri sayfasına geri dönebilirsiniz. Ayrıca, doğrudan bölümün dahili arama motorunu kullanarak da kelime arayabilirsiniz. 18 dilde örnekler bulacaksınız.</p>
-                                    </div>
-                                    <div class="box">
-                                        <h2>Vizyonumuz</h2>
-                                        <hr class="inner-line" />
-                                        <p>u bölüm kelimeleri doğru şekilde kullanmanıza yardımcı olacaktır. Bir kelimenin bağlam içinde nasıl kullanılacağını anlamak genellikle çeviriden daha kolaydır. Her sayfa birkaç örnek cümle ile gösterilmiştir ve istediğiniz zaman bir çeviri sayfasına geri dönebilirsiniz. Ayrıca, doğrudan bölümün dahili arama motorunu kullanarak da kelime arayabilirsiniz. 18 dilde örnekler bulacaksınız.Bu bölüm kelimeleri doğru şekilde kullanmanıza yardımcı olacaktır. Bir kelimenin bağlam içinde nasıl kullanılacağını anlamak genellikle çeviriden daha kolaydır. Her sayfa birkaç örnek cümle ile gösterilmiştir ve istediğiniz zaman bir çeviri sayfasına geri dönebilirsiniz. Ayrıca, doğrudan bölümün dahili arama motorunu kullanarak da kelime arayabilirsiniz. 18 dilde örnekler bulacaksınız.</p>
-                                    </div>
-                                </div>
-                            )}
-
+                        <div className="container-box">
+                            <img src="images/c3.png" />
+                            <h3>+90 538 029 2000</h3>
+                            <a href="*"> Bizi Arayın </a>
                         </div>
-                    </div>
-                </div>
-            )
-            }
+                    </section>
+
+                    <section className="about" id="about">
+                        <div className="about-img">
+                            <img src="images/boxImage3.jpg" />
+                        </div>
+
+                        <div className="about-text">
+                            <h2>İyi yaşamak, iyi beslenmekle başlar.</h2>
+                            <p>asdfa sdfqwefqw asdfkjs nkjqwnfjqwn efkjqwn efkjqwnekfjjjjjqkw jnkf qwejoıqwjo ıqjwfoıejfoqwıfj salkdqlwknelrqw oıjqwofıqwjeq qkwenlqkwe</p>
+                            <a href="#" className="btn">Devamını Gör</a>
+                        </div>
+                    </section>
+
+                </>
+
+
+
+            )}
         </div>
     )
 }
