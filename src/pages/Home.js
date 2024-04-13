ca62008cfa3cd96d3ac505e2ae32febfab57ffb3
Oğuz Akçay <98681852+akcy01@users.noreply.github.com>
2024-04-13 18:32:55 +0300

    HomePage has completed. Products has refactored.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -43,7 +43,7 @@ function Home(props) {
                 <>
                     <section className="home" id="home">
                         <div className="home-text">
-                            <h1>Gelin Yiyin ve gerçek lezzetin tadını çıkarın. </h1>
+                            <h1>Buyrun, misafirimiz olun ve gerçek lezzetin tadını çıkarın.</h1>
                             <a href="#" className="btn">Devamını Gör</a>
                         </div>
 
@@ -78,23 +78,30 @@ function Home(props) {
                         </div>
 
                         <div className="about-text">
-                            <h2>İyi yaşamak, iyi beslenmekle başlar.</h2>
-                            <p>asdfa sdfqwefqw asdfkjs nkjqwnfjqwn efkjqwn efkjqwnekfjjjjjqkw jnkf qwejoıqwjo ıqjwfoıejfoqwıfj salkdqlwknelrqw oıjqwofıqwjeq qkwenlqkwe</p>
+                            <h2>İyi yaşamak, İyi beslenmekle başlar.</h2>
+                            <p>Tavuklarımızı özenle seçiyor ve en titiz standartlara göre işliyoruz. En taze ve doğal malzemeleri kullanarak, sağlığınızı ve damak zevkinizi düşünerek hazırlıyoruz. Her bir ürünümüz, sağlık ve hijyen standartlarına tam uyumluluk göstermektedir.
+
+                            </p>
+                            <p>Dükkanımızda sunulan tavuk çeşitleri arasında, çıtır tavuk, ızgara tavuk, tavuk kanat, tavuk but, tavuk göğsü gibi birçok seçenek bulunmaktadır. Ayrıca sağlıklı yaşamı destekleyen salatalar ve lezzetli mezeler de menümüzde yer almaktadır.</p>
+                            <p>
+                            Müşteri memnuniyeti ve güveni bizim için her zaman önceliklidir. Tavuk ürünlerimizde en yüksek kalite ve tazelik standartlarını korumak için sürekli çaba gösteriyoruz. Müşterilerimize en iyi hizmeti sunmak ve onların sağlıklı beslenme ihtiyaçlarını karşılamak için var gücümüzle çalışıyoruz.
+                            </p>
+                            <p>Sağlıklı ve lezzetli tavuk ürünlerini keşfetmek ve deneyimlemek için sizi dükkanımıza bekliyoruz. Tadını çıkarın ve sağlıklı bir yaşam tarzına adım atın!</p>
                             <a href="#" className="btn">Devamını Gör</a>
                         </div>
                     </section>
 
                     <section className="mission" id="mission">
                         <div className="middle-text">
-                            <h4>Misyonumuz</h4>
-                            <h2>Lets check our mission and vision</h2>
+                            <h4>Misyonumuz & Vizyonumuz</h4>
+                            <h2>Kuruluşumuzdan bu yana, kalitemizden ödün vermeden sizlere en sağlıklı hizmeti sunmaktayız...</h2>
                         </div>
 
                         <div className="mission-content">
                             <div className="row">
                                 <img src="images/boxImage1.jpg" />
                                 <h3>Misyonumuz</h3>
-                                <p>asdfqaet qwdfqwefkj qkwjnefqkwjfoı jasdjfoqwepo  mksadlf</p>
+                                <p>En taze tavukları ve sağlıklı yan ürünleriyle müşterilerimize lezzetli bir deneyim sunuyoruz. Kalite ve güvenilirlik ilkelerimizle her zaman en iyisini hedefliyoruz.</p>
                                 <div className="in-text">
                                     <div className="s-btnn">
                                         <a href="#"> Devamını Gör </a>
@@ -105,7 +112,7 @@ function Home(props) {
                             <div className="row">
                                 <img src="images/boxImage2.jpg" />
                                 <h3>Vizyonumuz</h3>
-                                <p>asdfqaet qwdfqwefkj qkwjnefqkwjfoı jasdjfoqwepo  mksadlf</p>
+                                <p>Müşterilerimize en taze ve en lezzetli tavuk ürünlerini sunarak, herkesin sağlıklı beslenme tercihlerine destek olmayı ve keyifli bir yemek deneyimi yaşatmayı amaçlıyoruz. </p>
                                 <div className="in-text">
                                     <div className="s-btnn">
                                         <a href="#"> Devamını Gör </a>
@@ -116,7 +123,7 @@ function Home(props) {
                             <div className="row">
                                 <img src="images/bakedBoxImage4.jpg" />
                                 <h3>Hedeflerimiz</h3>
-                                <p>asdfqaet qwdfqwefkj qkwjnefqkwjfoı jasdjfoqwepo  mksadlf</p>
+                                <p>Hedeflerimiz doğrultusunda, her geçen gün daha fazla müşteriye ulaşmayı ve onların sağlıklı beslenme alışkanlıklarına katkıda bulunmayı amaçlıyoruz.  </p>
                                 <div className="in-text">
                                     <div className="s-btnn">
                                         <a href="#"> Devamını Gör </a>
@@ -126,7 +133,7 @@ function Home(props) {
                         </div>
 
                         <div className="row-btn">
-                            <a href="#" className="btn"> Devamını Gör </a>
+                            <a href="#" className="btn"> Hakkımızda </a>
                         </div>
                     </section>
                 </>
