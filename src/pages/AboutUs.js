6d5aba66feb2826092d358e6370ad1ae59046b04
Oğuz Akçay <98681852+akcy01@users.noreply.github.com>
2024-04-14 03:01:52 +0300

    AboutUs responsive design has completed.

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -16,13 +16,23 @@ function AboutUs() {
 
         <div className='about-text'>
           <h2>Hikayemiz</h2>
-          <p>1997 yılında Mehmet Akçay tarafından kurulan iş yerimiz, o günden bu yana kaliteden ödün vermeden Banvit ürünleriyle toptan ve perakende satış yapmaktadır. Fatih Caddesi'nde başlayan bu serüven, zamanla ailemizin bir parçası olan Mete Akçay'ın katılımıyla daha da güçlendi. Babamız Mete Akçay'ın yetiştirilmesiyle birlikte iş yerimizi birlikte yönetmeye başladık.
+          <p>1997 yılında Mehmet Akçay tarafından kurulan iş yerimiz, o günden bu yana kaliteden ödün vermeden Banvit ürünleriyle toptan ve perakende satış yapmaktadır. Fatih Caddesi'nde başlayan bu serüven, zamanla ailemizin bir parçası olan Mete Akçay'ın katılımıyla daha da güçlendi. Mete Akçay'ın yetişmesiyle birlikte iş yerimiz birlikte yönetilmeye başlandı.
+          </p>
+          <p> Deprem sonrası bir dönüm noktası yaşadık ve Liwan Hotel'in sahipleriyle işbirliği yaparak işimizi daha da büyüttük. Bugün, bu işbirliği sayesinde daha geniş bir kitleye ulaşıyor ve müşterilerimize daha iyi hizmet sunabiliyoruz.</p>
+          <p>  Geçmişimizden aldığımız güçle ve geleceğe olan inancımızla, müşterilerimize en iyi hizmeti sunmak için sürekli olarak çalışıyoruz. Müşteri memnuniyetini ve kalite standartlarını ön planda tutarak, her geçen gün daha da ileriye taşımayı hedefliyoruz.</p>
+          <p>     Siz değerli müşterilerimizle birlikte, bu yolculuğumuzu devam ettirmekten büyük mutluluk duyuyoruz. Teşekkürlerimizle..</p>
+        </div>
+      </section>
 
-            Deprem sonrası bir dönüm noktası yaşadık ve Liwan Hotel'in sahipleriyle işbirliği yaparak işimizi daha da büyüttük. Bugün, bu işbirliği sayesinde daha geniş bir kitleye ulaşıyor ve müşterilerimize daha iyi hizmet sunabiliyoruz.
-
-            Geçmişimizden aldığımız güçle ve geleceğe olan inancımızla, müşterilerimize en iyi hizmeti sunmak için sürekli olarak çalışıyoruz. Müşteri memnuniyetini ve kalite standartlarını ön planda tutarak, her geçen gün daha da ileriye taşınmayı hedefliyoruz.
-
-            Siz değerli müşterilerimizle birlikte, bu yolculuğumuzu devam ettirmekten büyük mutluluk duyuyoruz. Teşekkürlerimizle.</p>
+      <section id='trust'>
+        <h1>Çalıştığımız Markalar</h1>
+        <p>Sizlere sunduğumuz ürünlerin markaları..</p>
+        <div className='trust-img'>
+            <img src='images/banvitLogo.png' alt='banvitLogo'/>
+            <img src='images/superFresh.png' alt='superFreshLogo'/>
+            <img src='images/cocaColaLogo.png' alt='cocaColaLogo'/>
+            <img src='images/spriteLogo.png' alt='spriteLogo'/>
+            <img src='images/inisDibi.png' alt='inisDibi'/>
         </div>
       </section>
     </>
