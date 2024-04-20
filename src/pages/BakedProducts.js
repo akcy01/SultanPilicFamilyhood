7bceb5375ae6315f9c2263da808d6ba7082008be
Oğuz Akçay <98681852+akcy01@users.noreply.github.com>
2024-04-20 14:21:14 +0300

    bakedProducts page has copmleted.

--- src/pages/BakedProducts.js
+++ src/pages/BakedProducts.js
@@ -19,6 +19,7 @@ function BakedProducts() {
 
     return (
         <>
+
             <div className='baked-container'>
                 <h3 className='baked-title'> Pişmiş Ürünler </h3>
                 <div className='products-container'>
@@ -27,32 +28,32 @@ function BakedProducts() {
                         <h3>Örnek Ürün</h3>
                     </div>
 
-                    <div className='product' data-name="p-2" onClick={() => showPreview('p-1')}>
+                    <div className='product' data-name="p-2" onClick={() => showPreview('p-2')}>
                         <img src='images/bakedProducts/KanatSis.jpg' />
                         <h3>Örnek Ürün</h3>
                     </div>
 
-                    <div className='product' data-name="p-3" onClick={() => showPreview('p-1')}>
+                    <div className='product' data-name="p-3" onClick={() => showPreview('p-3')}>
                         <img src='images/bakedProducts/KanatSis.jpg' />
                         <h3>Örnek Ürün</h3>
                     </div>
 
-                    <div className='product' data-name="p-4" onClick={() => showPreview('p-1')}>
+                    <div className='product' data-name="p-4" onClick={() => showPreview('p-4')}>
                         <img src='images/bakedProducts/KanatSis.jpg' />
                         <h3>Örnek Ürün</h3>
                     </div>
 
-                    <div className='product' data-name="p-5" onClick={() => showPreview('p-1')}>
+                    <div className='product' data-name="p-5" onClick={() => showPreview('p-5')}>
                         <img src='images/bakedProducts/KanatSis.jpg' />
                         <h3>Örnek Ürün</h3>
                     </div>
 
-                    <div className='product' data-name="p-6" onClick={() => showPreview('p-1')}>
+                    <div className='product' data-name="p-6" onClick={() => showPreview('p-6')}>
                         <img src='images/bakedProducts/KanatSis.jpg' />
                         <h3>Örnek Ürün</h3>
                     </div>
 
-                    <div className='product' data-name="p-7" onClick={() => showPreview('p-1')}>
+                    <div className='product' data-name="p-7" onClick={() => showPreview('p-7')}>
                         <img src='images/bakedProducts/KanatSis.jpg' />
                         <h3>Örnek Ürün</h3>
                     </div>
@@ -95,14 +96,14 @@ function BakedProducts() {
                     <p>Örnek ürünümüz özenle hazırlanır ve en taze pişmiş şekliyle sizlere sunulur.</p>
                 </div>
 
-                <div className={`preview ${activePreview === 'p-6' ? 'active' : ''}`} data-target="p-">
+                <div className={`preview ${activePreview === 'p-6' ? 'active' : ''}`} data-target="p-6">
                     <GrClose className='close-icon' onClick={closePreview} />
                     <img src='images/bakedProducts/KanatSis.jpg' />
                     <h3>Örnek Ürün</h3>
                     <p>Örnek ürünümüz özenle hazırlanır ve en taze pişmiş şekliyle sizlere sunulur.</p>
                 </div>
 
-                <div className={`preview ${activePreview === 'p-7' ? 'active' : ''}`} data-target="p-1">
+                <div className={`preview ${activePreview === 'p-7' ? 'active' : ''}`} data-target="p-7">
                     <GrClose className='close-icon' onClick={closePreview} />
                     <img src='images/bakedProducts/KanatSis.jpg' />
                     <h3>Örnek Ürün</h3>
@@ -114,27 +115,3 @@ function BakedProducts() {
 }
 
 export default BakedProducts
-
-
-// let previewContainer = document.querySelector('.products-preview');
-// let previewBox = previewContainer.querySelectorAll('.preview')
-
-// document.querySelectorAll('.products-container .product').forEach(product => {
-//     product.onclick = () => {
-//         previewContainer.computedStyleMap.display = 'flex';
-//         let name = product.getAttribute('data-name');
-//         previewBox.forEach(preview => {
-//             let target = preview.getAttribute('data-target');
-//             if (name == target) {
-//                 preview.classList.add('active');
-//             }
-//         })
-//     }
-// })
-
-// previewBox.forEach(close => {
-//     close.querySelector('.fa-times').onclick = () => {
-//         close.classList.remove('active');
-//         previewContainer.computedStyleMap.display = 'none';
-//     }
-// })
